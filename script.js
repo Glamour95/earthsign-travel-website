d96d8db60541293f5991a84d57522a73b241498d
Glamour <118850729+Glamour95@users.noreply.github.com>
2026-08-05 04:31:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,26 @@ header.classList.remove("scrolled");
 }
 
 });
+// Fade In Sections
+
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries)=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+sections.forEach(section=>{
+
+observer.observe(section);
+
+});
